9b71d306ecda0d973c77dc1ff7b5d9721c8016d3
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-26 16:55:12 +0800

    Update NextJS app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '@/styles/globals.scss';
+import 'material-symbols';
+import { config as fontAwesomeConfig } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
 import { roboto, inter } from '@/utils/fonts';
 import type { ReactElement, ReactNode } from 'react';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+
+fontAwesomeConfig.autoAddCss = false;
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
