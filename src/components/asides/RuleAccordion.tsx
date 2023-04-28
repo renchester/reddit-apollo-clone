48f4264e14aea12987a0f16b07a8d0266f595c5f
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-28 18:58:00 +0800

    Add SubredditRules component and styles

---
+++ src/components/asides/RuleAccordion.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import styles from './Aside.module.scss';
+
+type RuleAccordionProps = {
+  index: number;
+  title: string;
+  details: string;
+};
+
+function RuleAccordion(props: RuleAccordionProps) {
+  const { index, title, details } = props;
+
+  const [isExpanded, setExpandedState] = useState(false);
+
+  const toggleRule = () => setExpandedState((prev) => !prev);
+
+  return (
+    <li className={styles.rule}>
+      <span className={styles.rule__index}>{index}.</span>
+      <p className={styles.rule__title}>{title}</p>
+      <button
+        type="button"
+        aria-label="Show rule details"
+        onClick={toggleRule}
+        className={styles.rule__btnExpand}
+      >
+        <span className={`material-symbols-outlined`}>
+          {isExpanded ? 'expand_less' : 'expand_more'}
+        </span>
+      </button>
+      {isExpanded && (
+        <div className={styles.rule__expanded}>
+          <p className={styles.rule__details}>{details}</p>
+        </div>
+      )}
+    </li>
+  );
+}
+export default RuleAccordion;
--- src/components/asides/SubredditRules.tsx
+++ src/components/asides/SubredditRules.tsx
@@ -1,16 +1,34 @@
 import styles from './Aside.module.scss';
+import RuleAccordion from './RuleAccordion';
 
 function SubredditRules() {
   return (
     <aside className={styles.aside__main}>
       <h3>r/All Rules</h3>
       <ul>
-        <li>Don&apos;t do this</li>
-        <li>Don&apos;t do that</li>
-        <li>Don&apos;t do what</li>
-        <li>Don&apos;t do this</li>
+        <RuleAccordion
+          index={1}
+          title="Don't do this"
+          details="Humor is subjective, but all posts must at least make an attempt at humor. Posts which are intentionally disruptive, inane, or nonsensical will be removed."
+        />
+        <RuleAccordion
+          index={2}
+          title="Don't do this"
+          details="No personally identifying information, including anything hosted on platforms making that information public. Posts encouraging the harassment of any individual, group, community, or subreddit will be removed and may result in a ban. If necessary, a report will be made to the site administration. In accordance with Reddit's policies, there is zero tolerance for this."
+        />
+        <RuleAccordion
+          index={3}
+          title="Make sure to do this"
+          details="Anything which involves or includes politics or a political figure – even if they are not the focus of the post – may not be posted here."
+        />
+        <RuleAccordion
+          index={4}
+          title="You can probably break this rule"
+          details="Yes like we said do not do this"
+        />
       </ul>
     </aside>
   );
 }
+
 export default SubredditRules;
