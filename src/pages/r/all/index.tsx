656744ed67dcd153b4337bb02d25a65b308d9123
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-29 15:16:17 +0800

    Add SubredditMenu to SubredditPage

--- src/pages/r/all/index.tsx
+++ src/pages/r/all/index.tsx
@@ -2,13 +2,17 @@ import styles from './SubredditPage.module.scss';
 import Head from 'next/head';
 import MasterLayout from '@/layouts/MasterLayout';
 import FeedPageLayout from '@/layouts/FeedPageLayout';
-import { type ReactElement } from 'react';
+import { useState, type ReactElement } from 'react';
 import AsideContainer from '@/components/asides/AsideContainer';
 import PostPreview from '@/components/posts/PostPreview';
 import SubredditAside from '@/components/asides/SubredditAside';
-import SubredditRules from '@/components/asides/SubredditRules';
+import SubredditMenu from '@/components/menus/SubredditMenu';
 
 function SubredditPage() {
+  const [isMenuShown, setMenuVisibility] = useState(false);
+
+  const toggleMenu = () => setMenuVisibility((prev) => !prev);
+
   return (
     <>
       <Head>
@@ -23,13 +27,32 @@ function SubredditPage() {
               very own.
             </p>
 
-            <button
-              type="button"
-              aria-label="Subscribe to this subreddit and join the community"
-              className={styles.btnJoin}
-            >
-              Join
-            </button>
+            <div className={styles.btn__container}>
+              <button
+                type="button"
+                aria-label="Subscribe to this subreddit and join the community"
+                className={styles.btn__join}
+              >
+                Join
+              </button>
+              <button
+                type="button"
+                aria-label="Show subreddit options"
+                aria-haspopup
+                aria-expanded={isMenuShown}
+                className={styles.btn__options}
+                onClick={toggleMenu}
+              >
+                <i
+                  className={`material-symbols-outlined ${styles.btn__icon}`}
+                  aria-hidden
+                >
+                  more_horiz
+                </i>
+              </button>
+
+              {isMenuShown && <SubredditMenu />}
+            </div>
           </div>
           <PostPreview id="4" />
           <PostPreview id="5" />
