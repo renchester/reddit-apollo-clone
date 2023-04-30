423ee2a6a334d673aba137740e147232d811c193
Renchester Ramos <renchesterjramos@gmail.com>
2023-04-30 17:22:51 +0800

    Update labels for menu buttons

--- src/components/subreddits/SubredditMenu.tsx
+++ src/components/subreddits/SubredditMenu.tsx
@@ -4,10 +4,21 @@ import MenuButton from '../menus/MenuButton';
 function SubredditMenu() {
   return (
     <Menu>
-      <MenuButton icon="history_edu" text="Submit Post" />
-      <MenuButton icon="history_edu" text="Submit Post" />
-      <MenuButton icon="favorite" text="Subscribe" />
-      <MenuButton icon="star" text="Favorite" />
+      <MenuButton
+        icon="history_edu"
+        text="Submit Post"
+        label="Submit a post to this subreddit"
+      />
+      <MenuButton
+        icon="favorite"
+        text="Subscribe"
+        label="Subscribe to this subreddit"
+      />
+      <MenuButton
+        icon="star"
+        text="Favorite"
+        label="Make this subreddit a favorite"
+      />
     </Menu>
   );
 }
