101b2f7aed745cd1b9971bfd4552df80d7b0d98c
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-01 23:00:06 +0800

    Create addUserToDb function for new users

---
+++ src/firebase/auth/addUserToDb.ts
@@ -0,0 +1,31 @@
+import { Timestamp, doc, setDoc } from 'firebase/firestore';
+import { db } from '../config';
+
+type AddUserToDbProps = {
+  user_id: string;
+  username: string;
+  email: string;
+  date_created: string | Date;
+  provider: string;
+};
+
+const addUserToDb = async (props: AddUserToDbProps) => {
+  const { user_id, username, email, date_created, provider } = props;
+
+  try {
+    const userRef = doc(db, 'users', user_id);
+
+    await setDoc(userRef, {
+      username,
+      email,
+      provider,
+      date_created: Timestamp.fromDate(new Date(date_created)),
+    });
+
+    console.log('done setting up user');
+  } catch (e) {
+    console.error('Error: Problem setting new user in database');
+  }
+};
+
+export default addUserToDb;
