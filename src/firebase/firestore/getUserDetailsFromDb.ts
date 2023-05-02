059dbe0d42e3bc911cdcdc41e84c533ba0eb7a45
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-02 23:31:23 +0800

    Add getUserDetailsFromDb function

---
+++ src/firebase/firestore/getUserDetailsFromDb.ts
@@ -0,0 +1,15 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../config';
+import { User } from '@/types/types';
+
+const getUserDetailsFromDb = async (userId: string) => {
+  const userRef = doc(db, 'users', userId);
+  const docSnap = await getDoc(userRef);
+
+  if (docSnap.exists()) {
+    const userData = docSnap.data() as User;
+    return userData;
+  } else return null;
+};
+
+export default getUserDetailsFromDb;
