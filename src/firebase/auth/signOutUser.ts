c1e41a2f2842c4d43b9996231341f27ee0e0560b
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-02 23:29:45 +0800

    Make signOutUser function async

--- src/firebase/auth/signOutUser.ts
+++ src/firebase/auth/signOutUser.ts
@@ -1,8 +1,18 @@
 import { auth } from '../config';
 import { signOut } from 'firebase/auth';
+import { FirebaseError } from 'firebase/app';
 
-const signOutUser = () => {
-  signOut(auth);
+const signOutUser = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    if (error instanceof FirebaseError) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+      console.error(`ERROR ${errorCode}: ${errorMessage}`);
+    }
+  }
 };
 
 export default signOutUser;
