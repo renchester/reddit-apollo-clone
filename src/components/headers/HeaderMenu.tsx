caa18662025628072316d3c223b450e95c3e7c8f
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-02 23:35:43 +0800

    Update HeaderMenu component content and styles

--- src/components/headers/HeaderMenu.tsx
+++ src/components/headers/HeaderMenu.tsx
@@ -1,13 +1,35 @@
 import styles from './HeaderMenu.module.scss';
+import Link from 'next/link';
 import Switch from '../buttons/Switch';
 import { useTheme } from '@/hooks/useTheme';
+import { useAuth } from '@/hooks/useAuth';
+import signOutUser from '@/firebase/auth/signOutUser';
 
 function HeaderMenu() {
   const { isDark, toggleTheme } = useTheme();
+  const { user } = useAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await signOutUser();
+    } catch (e) {
+      if (e instanceof Error) {
+        console.error(e.message);
+      }
+    }
+  };
 
   return (
     <div className={styles.menu} id="header-menu">
       <ul role="menu" className={styles.menu__list}>
+        {user && (
+          <li className={styles.menu__listItem}>
+            <Link href="/account" className={styles.menu__label}>
+              {user.username}
+            </Link>
+          </li>
+        )}
+
         <li className={styles.menu__listItem}>
           <label
             htmlFor="header-menu__theme-switcher"
@@ -18,11 +40,29 @@ function HeaderMenu() {
           <Switch
             id="header-menu__theme-switcher"
             ariaLabel="Switch to Light/Dark Mode"
-            isOn={isDark}
+            isOn={isDark || false}
             handler={toggleTheme}
           />
         </li>
-        <li></li>
+        <li className={styles.menu__listItem}>
+          {user ? (
+            <button
+              type="button"
+              className={styles.menu__label}
+              onClick={handleSignOut}
+            >
+              Sign out
+            </button>
+          ) : (
+            <Link
+              href="/account/login"
+              className={styles.menu__label}
+              onClick={() => console.log('signing in')}
+            >
+              Log in
+            </Link>
+          )}
+        </li>
       </ul>
     </div>
   );
