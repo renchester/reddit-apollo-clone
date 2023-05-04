63ac356fa800a438cac5bae4123d2d239f44f419
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-05 04:01:13 +0800

    Add snapshot listener for user subscriptions and change state structure

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,91 +1,78 @@
-import { auth } from '@/firebase/config';
-import getUserDetailsFromDb from '@/firebase/firestore/getUserDetailsFromDb';
+import { auth, db } from '@/firebase/config';
+import getUserDetailsFromDb from '@/firebase/firestore/user/getUserDetailsFromDb';
 import { onAuthStateChanged } from 'firebase/auth';
-import type { User } from '@/types/types';
+import type { UserSubscription } from '@/types/types';
 import {
   type ReactNode,
   createContext,
-  useState,
   useContext,
   useEffect,
-  useReducer,
   useMemo,
+  useState,
 } from 'react';
+import { User } from '@/types/types';
+import { collection, onSnapshot } from 'firebase/firestore';
 
 type AuthContextType = {
   user: User | null;
-  userDispatch: React.Dispatch<Action>;
+  subscriptions: UserSubscription[] | null;
 };
 
 type AuthProviderProps = {
   children: ReactNode;
 };
 
-type Action =
-  | { type: 'set_user_details'; userDetails: User }
-  | { type: 'auth_off' }
-  | { type: 'upvote_comment'; payload: string }
-  | { type: 'downvote_comment'; payload: string };
-
 const AuthContext = createContext<AuthContextType | null>(null);
-
-const userReducer = (state: User | null, action: Action) => {
-  switch (action.type) {
-    case 'set_user_details': {
-      if (action.userDetails === null) {
-        return state;
-      } else if (state === null) {
-        return { ...action.userDetails };
-      } else return Object.assign(state, action.userDetails);
-    }
-
-    case 'auth_off': {
-      return null;
-    }
-
-    case 'upvote_comment': {
-      return state;
-    }
-
-    case 'downvote_comment': {
-      return state;
-    }
-
-    default: {
-      console.error(`Error: Unhandled action type`);
-      return state;
-    }
-  }
-};
 
 export const AuthProvider = (props: AuthProviderProps) => {
   const { children } = props;
 
-  const [user, userDispatch] = useReducer(userReducer, null);
+  const [user, setUser] = useState<User | null>(null);
+  const [subscriptions, setSubscriptions] = useState<UserSubscription[] | null>(
+    null,
+  );
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-      if (user) {
-        const userDetails = await getUserDetailsFromDb(user.uid);
-        console.log(userDetails);
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+      if (firebaseUser) {
+        const userDetails = await getUserDetailsFromDb(firebaseUser.uid);
 
         if (userDetails) {
           // Set user to stored user details from Firestore
-          userDispatch({ type: 'set_user_details', userDetails });
-
-          // Set user interactions
+          setUser(userDetails);
         } else {
           console.error('ERROR: Unable to set user details');
+          setUser(null);
         }
       } else {
-        userDispatch({ type: 'auth_off' });
+        setUser(null);
       }
     });
 
     return () => unsubscribe();
   }, []);
 
-  const value = useMemo(() => ({ user, userDispatch }), [user, userDispatch]);
+  useEffect(() => {
+    if (!user) {
+      setSubscriptions(null);
+      return;
+    }
+
+    const subscriptionsRef = collection(
+      db,
+      `users/${user.user_id}/subscribed_subreddits`,
+    );
+
+    const unsubscribeSubscriptions = onSnapshot(subscriptionsRef, (snapshot) =>
+      setSubscriptions(
+        snapshot.docs.map((doc) => doc.data() as UserSubscription),
+      ),
+    );
+
+    return () => unsubscribeSubscriptions();
+  }, [user]);
+
+  const value = useMemo(() => ({ user, subscriptions }), [user, subscriptions]);
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
