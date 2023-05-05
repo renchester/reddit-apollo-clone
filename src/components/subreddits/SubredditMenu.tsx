dedd2f819b9c089e4d45fa25ef201bd7abf05e93
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-06 03:51:33 +0800

    Add functionality to SubredditMenu buttons

--- src/components/subreddits/SubredditMenu.tsx
+++ src/components/subreddits/SubredditMenu.tsx
@@ -1,24 +1,64 @@
+import { Subreddit } from '@/types/types';
 import Menu from '../menus/Menu';
 import MenuButton from '../menus/MenuButton';
+import { useRouter } from 'next/router';
+import { useAuth } from '@/hooks/useAuth';
+import { useSnackbar } from '@/hooks/useSnackbar';
+import toggleFavoriteSubreddit from '@/firebase/firestore/subreddits/toggleFavoriteSubreddit';
 
-function SubredditMenu() {
+type SubredditMenuProps = {
+  subreddit: Subreddit;
+  isUserSubscribed: boolean;
+  handleSubscriptionChange: () => void;
+  isFavorited: boolean;
+};
+
+function SubredditMenu(props: SubredditMenuProps) {
+  const { subreddit, isUserSubscribed, handleSubscriptionChange, isFavorited } =
+    props;
+  const { user } = useAuth();
+  const { addAlert } = useSnackbar();
+  const router = useRouter();
+
+  const subscriptionText = isUserSubscribed ? 'Leave' : 'Subscribe';
+  const favoriteText = isFavorited ? 'Unfavorite' : 'Favorite';
+
+  const goToSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    router.push(`/r/${subreddit.name}/submit`);
+  };
+
+  const handleToggleFavorite = async () => {
+    if (!user) {
+      addAlert({
+        message: 'Favoriting subreddits is only available for logged in users',
+        status: 'error',
+      });
+    } else await toggleFavoriteSubreddit(user, subreddit.name);
+  };
+
   return (
     <Menu>
       <MenuButton
         icon="history_edu"
         text="Submit Post"
         label="Submit a post to this subreddit"
+        handler={goToSubmit}
       />
       <MenuButton
         icon="favorite"
-        text="Subscribe"
+        text={subscriptionText}
         label="Subscribe to this subreddit"
+        handler={handleSubscriptionChange}
       />
-      <MenuButton
-        icon="star"
-        text="Favorite"
-        label="Make this subreddit a favorite"
-      />
+      {isUserSubscribed && (
+        <MenuButton
+          icon="star"
+          text={favoriteText}
+          label="Make this subreddit a favorite"
+          handler={handleToggleFavorite}
+        />
+      )}
     </Menu>
   );
 }
