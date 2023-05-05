c15004da72f7443f36a4f43b6916c1b0056cad1f
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-05 20:21:56 +0800

    Create subreddit-deleting functions

---
+++ src/firebase/firestore/subreddits/deleteSubreddit.ts
@@ -0,0 +1,28 @@
+import { db } from '@/firebase/config';
+import { Subreddit } from '@/types/types';
+import { deleteDoc, doc, getDoc } from 'firebase/firestore';
+import forceDeleteSubscription from './forceDeleteSubscription';
+
+const deleteSubreddit = async (name: string) => {
+  try {
+    const subredditRef = doc(db, 'subreddits', name);
+    const subSnap = await getDoc(subredditRef);
+
+    if (subSnap.exists()) {
+      const subData = subSnap.data() as Subreddit;
+      const members = subData.members;
+
+      await deleteDoc(subredditRef);
+
+      for await (const userId of members) {
+        await forceDeleteSubscription(userId, subData.subreddit_id);
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+};
+
+export default deleteSubreddit;
---
+++ src/firebase/firestore/subreddits/forceDeleteSubscription.ts
@@ -0,0 +1,37 @@
+import { collection, getDocs, deleteDoc, doc } from 'firebase/firestore';
+import { db } from '@/firebase/config';
+import { UserSubscription } from '@/types/types';
+
+const forceDeleteSubscription = async (userId: string, subredditId: string) => {
+  try {
+    const subscriptionRef = collection(
+      db,
+      `users/${userId}/subscribed_subreddits`,
+    );
+    const snapshot = await getDocs(subscriptionRef);
+    let subscriptionToDelete = '';
+
+    snapshot.forEach((doc) => {
+      const subscription = doc.data() as UserSubscription;
+
+      if (subscription.subreddit_id === subredditId) {
+        subscriptionToDelete = subscription.subreddit;
+      }
+    });
+
+    if (subscriptionToDelete) {
+      await deleteDoc(
+        doc(
+          db,
+          `users/${userId}/subscribed_subreddits/${subscriptionToDelete}`,
+        ),
+      );
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+  }
+};
+
+export default forceDeleteSubscription;
