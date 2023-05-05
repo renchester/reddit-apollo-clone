b672f7d3cdcf5e8720c102fca5d5c3fa096b5c4b
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-06 03:58:47 +0800

    Update configuration files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/reddit-apollo-clone.appspot.com/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
