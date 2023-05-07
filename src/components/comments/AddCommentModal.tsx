6525d256701680a0c96db3698b78fc92ff7134ac
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-08 01:11:47 +0800

    Update AddCommentModal contents and styles

--- src/components/comments/AddCommentModal.tsx
+++ src/components/comments/AddCommentModal.tsx
@@ -1,8 +1,66 @@
-import { useNewComment } from '@/hooks/useNewComment';
 import styles from './AddCommentModal.module.scss';
+import { useNewComment } from '@/hooks/useNewComment';
+import { Comment, Post } from '@/types/types';
+import { useEffect, useState } from 'react';
+import { useSnackbar } from '@/hooks/useSnackbar';
+import { useAuth } from '@/hooks/useAuth';
+import submitComment from '@/firebase/firestore/comments/create/submitComment';
 
-function AddCommentModal() {
-  const { hideCommentModal } = useNewComment();
+type AddCommentModalProps = {
+  parentPost: Post;
+  parentComment?: Comment | null;
+};
+
+function AddCommentModal(props: AddCommentModalProps) {
+  const { parentComment, parentPost } = props;
+  const { user } = useAuth();
+  const { hideCommentModal, setParentComment } = useNewComment();
+  const { addAlert } = useSnackbar();
+
+  const [comment, setComment] = useState('');
+  const commentLevel = parentComment ? parentComment.comment_level + 1 : 1;
+
+  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmitComment = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ parentComment, parentPost, comment, commentLevel });
+
+    try {
+      if (!user) throw new Error('Only logged in users may submit a comment');
+
+      if (!parentPost) throw new Error('Comment must have a parent post');
+
+      const result = await submitComment(
+        user,
+        { content: comment, level: commentLevel },
+        parentPost,
+        parentComment?.comment_id,
+      );
+
+      if (result) {
+        hideCommentModal();
+        addAlert({ message: 'Comment added', status: 'success' });
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        addAlert({
+          message: error.message,
+          status: 'error',
+        });
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (parentComment) {
+      setParentComment(parentComment);
+    }
+
+    return () => setParentComment(null);
+  }, [parentComment, setParentComment]);
 
   return (
     <div className={styles.modal}>
@@ -15,7 +73,7 @@ function AddCommentModal() {
         </i>
       </button>
       <h2 className={styles.modal__heading}>New Comment</h2>
-      <form action="" className={styles.modal__form}>
+      <form className={styles.modal__form}>
         <label htmlFor="comment_text" className={styles.modal__label}>
           Type out your comment
         </label>
@@ -25,9 +83,35 @@ function AddCommentModal() {
           className={styles.modal__textarea}
           placeholder="Enter your comment here"
           minLength={1}
-          required
+          onChange={handleCommentChange}
+          value={comment}
         />
-        <button type="submit" className={styles.modal__btnSubmit}>
+
+        <div className={styles.reply}>
+          <span className={styles.reply__to}>
+            <i
+              className={`material-symbols-outlined ${styles.reply__icon}`}
+              aria-hidden
+            >
+              subdirectory_arrow_right
+            </i>
+            Replying to {parentComment ? 'comment' : 'post'}:
+          </span>
+          {parentComment ? (
+            <div className={styles.reply__content}>{parentComment.content}</div>
+          ) : (
+            <div className={styles.reply__content}>
+              &quot;{parentPost?.title}&quot;{' '}
+            </div>
+          )}
+        </div>
+
+        <button
+          type="submit"
+          className={styles.modal__btnSubmit}
+          disabled={comment.length < 1}
+          onClick={handleSubmitComment}
+        >
           Submit comment
         </button>
       </form>
