bca515e3e2d12ca33bb5b46c9c8c1f1a6f2a8422
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-08 01:18:16 +0800

    Update CommentFeed contents and styles

--- src/components/comments/CommentFeed.tsx
+++ src/components/comments/CommentFeed.tsx
@@ -1,37 +1,24 @@
 import styles from './CommentFeed.module.scss';
 import Comment from './Comment';
-import Overlay from '../Overlay';
-import AddCommentModal from './AddCommentModal';
 import { useNewComment } from '@/hooks/useNewComment';
 
 function CommentFeed() {
-  const { isCommentModalShown, hideCommentModal } = useNewComment();
+  const { comments } = useNewComment();
+
+  const topLevelComments = comments.filter(
+    (comment) => comment.comment_level === 1,
+  );
 
   return (
     <>
       <div className={styles.comment__feed}>
-        <Comment level={1}>
-          <Comment level={2}>
-            <Comment level={3} />
-          </Comment>
-          <Comment level={2}>
-            <Comment level={3}>
-              <Comment level={4}>
-                <Comment level={5}>
-                  <Comment level={6}></Comment>
-                </Comment>
-              </Comment>
-            </Comment>
-          </Comment>
-          <Comment level={2}></Comment>
-        </Comment>
-        <Comment level={1} />
+        {topLevelComments.map((comment) => (
+          <Comment
+            key={`comment-post__${comment.comment_id}`}
+            comment={comment}
+          />
+        ))}
       </div>
-      {isCommentModalShown && (
-        <Overlay hideChildren={hideCommentModal}>
-          <AddCommentModal />
-        </Overlay>
-      )}
     </>
   );
 }
