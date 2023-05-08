d33d90668328db9af2cc0d93d047481aa23532c8
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-09 03:18:02 +0800

    Update content for HomePage component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,48 @@
 import Head from 'next/head';
-import Link from 'next/link';
-import Image from 'next/image';
+
 import MasterLayout from '@/layouts/MasterLayout';
 import FeedPageLayout from '@/layouts/FeedPageLayout';
-import type { ReactElement } from 'react';
-import PostPreview from '@/components/posts/PostPreview';
+import { ReactElement, useEffect, useState } from 'react';
 import AsideContainer from '@/components/asides/AsideContainer';
-import HomeAside from '@/components/asides/HomeAside';
+import AsideTemplate from '@/components/asides/AsideTemplate';
+import { useAuth } from '@/hooks/useAuth';
+import { Post } from '@/types/types';
+import { GetStaticProps } from 'next';
+import fetchAllPosts from '@/firebase/firestore/posts/read/fetchAllPosts';
+import PostPreview from '@/components/posts/PostPreview';
 
-function HomePage() {
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await fetchAllPosts(50);
+
+  return {
+    props: { posts },
+    revalidate: 60,
+  };
+};
+
+type HomePageProps = {
+  posts: Post[];
+};
+
+function HomePage(props: HomePageProps) {
+  const { posts } = props;
+  const { user, subscriptions } = useAuth();
+  const [homePosts, setHomePosts] = useState<Post[]>(posts);
+
+  useEffect(() => {
+    if (!user || !subscriptions) {
+      setHomePosts(posts);
+      return;
+    }
+    const subscriptionNames = subscriptions.map((sub) => sub.subreddit);
+
+    const userSubscribedPosts = posts.filter((post) =>
+      subscriptionNames.includes(post.parent_subreddit),
+    );
+
+    setHomePosts(userSubscribedPosts);
+  }, [user, posts, subscriptions]);
+
   return (
     <>
       <Head>
@@ -16,13 +50,20 @@ function HomePage() {
       </Head>
       <div className="page__container">
         <main className="page__feed">
-          <h1 className="page__title">Popular</h1>
-          <PostPreview id="1" />
-          <PostPreview id="2" />
-          <PostPreview id="3" />
+          <h1 className="page__title">Home</h1>
+          {homePosts.length > 0 ? (
+            homePosts.map((post) => (
+              <PostPreview key={`home-${post.post_id}`} post={post} />
+            ))
+          ) : (
+            <p>No posts yet</p>
+          )}
         </main>
         <AsideContainer>
-          <HomeAside />
+          <AsideTemplate
+            title="Home"
+            description="Your personal Reddit frontpage. Come here to check in with your favorite communities."
+          />
         </AsideContainer>
       </div>
     </>
