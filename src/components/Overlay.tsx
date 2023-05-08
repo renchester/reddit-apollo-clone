c65497a666fce1523302f043795bfa7b0b32e481
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-09 03:29:20 +0800

    Update Overlay component props and listeners

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,16 +1,30 @@
-import { type ReactNode } from 'react';
+import { useEffect, type ReactNode } from 'react';
 import { motion } from 'framer-motion';
 import styles from './Overlay.module.scss';
 
 type OverlayProps = {
   children: ReactNode;
-  hideChildren: () => void;
+  hideChildren: (e?: any) => void;
 };
 
 function Overlay(props: OverlayProps) {
   const { children, hideChildren } = props;
 
+  useEffect(() => {
+    function escKeyListener(e: KeyboardEvent) {
+      if (e.key === 'Escape') {
+        hideChildren();
+      }
+    }
+
+    window.addEventListener('keydown', escKeyListener);
+
+    return () => window.removeEventListener('keydown', escKeyListener);
+  }, [hideChildren]);
+
   const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+
     // Only run event when actual overlay is clicked
     if (e.target === e.currentTarget) {
       hideChildren();
