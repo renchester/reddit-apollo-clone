4387904b43a14fa27a42c49ac0230021350d5b6b
Renchester Ramos <renchesterjramos@gmail.com>
2023-05-10 01:37:03 +0800

    Add AboutPage component and styles

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,44 @@
+import styles from './AboutPage.module.scss';
+import Footer from '@/components/Footer';
+import FeedPageLayout from '@/layouts/FeedPageLayout';
+import MasterLayout from '@/layouts/MasterLayout';
+import Head from 'next/head';
+import { ReactElement } from 'react';
+
+function AboutPage() {
+  return (
+    <>
+      <Head>
+        <title>About - Reddit Clone</title>
+      </Head>
+      <div className="page__container">
+        <main className="page__feed">
+          <h2 className="page__title">About this site </h2>
+          <a
+            href="https://github.com/renchester/reddit-apollo-clone"
+            rel="noopener noreferrer"
+            target="_blank"
+            className={styles.link}
+          >
+            Check the Github repository of this app
+          </a>
+          <p className={styles.info}>
+            This Reddit clone is built using React + NextJS for the front-end,
+            Firebase for the back-end, and Sass for styling.
+          </p>
+        </main>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+AboutPage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <MasterLayout>
+      <FeedPageLayout label="About">{page}</FeedPageLayout>
+    </MasterLayout>
+  );
+};
+
+export default AboutPage;
