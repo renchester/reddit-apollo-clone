d46f7e38b24f0ee7243cb2a86df6508a2d72442a
Renchester Ramos <renchesterjramos@gmail.com>
2023-07-13 13:16:00 +0800

    Add 30-day account rule to subreddit creation

--- src/firebase/firestore/subreddits/create/addSubreddit.ts
+++ src/firebase/firestore/subreddits/create/addSubreddit.ts
@@ -1,10 +1,22 @@
 import { User } from '@/types/types';
 import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
 import { nanoid } from 'nanoid';
+import { differenceInDays } from 'date-fns';
 import { db } from '../../../config';
 import joinSubreddit from '../update/joinSubreddit';
 
 const addSubreddit = async (name: string, description: string, user: User) => {
+  // Check if user age is greater than 30 days
+  const userAgeInDays = differenceInDays(
+    new Date(),
+    new Date(user.date_created.toString()),
+  );
+
+  if (Math.abs(userAgeInDays) < 30)
+    throw new Error(
+      'Your account must be older than 30 days to create a subreddit',
+    );
+
   try {
     const subRef = doc(db, 'subreddits', name);
     const subredditId = `subreddit__${nanoid()}`;
